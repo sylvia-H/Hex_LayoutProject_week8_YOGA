be8fc25bb448ddb283418ec029771fb64af6f2fc
sylvia-H <sylvie11gaia@gmail.com>
2021-09-05 23:28:56 +0800

    draft

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,60 +1,9 @@
-(function() {
+const courseSwiper = new Swiper ('.courseSwiper', {
 
-  'use strict';
+  slidesPerView: 1.5,
+  spaceBetween: 30,
 
-  // breakpoint where swiper will be destroyed
-  // and switches to a dual-column layout
-  const breakpoint = window.matchMedia( '(max-width: 992px)' );
-
-  // keep track of swiper instances to destroy later
-  let courseSwiper;
-
-  //////////////////////////////////////////////////////////////////
-
-  const breakpointChecker = function() {
-
-    // if larger viewport and multi-row layout needed
-    if ( breakpoint.matches === true ) {
-
-      // clean up old instances and inline styles when available
-      if ( courseSwiper !== undefined ) courseSwiper.destroy( true, true );
-
-      // or/and do nothing
-      return;
-
-      // else if a small viewport and single column layout needed
-    } else if ( breakpoint.matches === false ) {
-
-      // fire small viewport version of swiper
-      return enableSwiper();
-
-    }
-
-  };
-
-  //////////////////////////////////////////////////////////////////
-
-  const enableSwiper = function() {
-
-    courseSwiper = new Swiper ('.swiper-container', {
-
-      slidesPerView: 1.5,
-      spaceBetween: 30,
-
-    });
-
-  };
-
-  //////////////////////////////////////////////////////////////////
-
-  // keep an eye on viewport size changes
-  breakpoint.addListener(breakpointChecker);
-
-  // kickstart
-  breakpointChecker();
-
-})(); /* IIFE end */
-
+});
 
 const customerSwiper = new Swiper(".customerSwiper",{
   slidesPerView: 1,
