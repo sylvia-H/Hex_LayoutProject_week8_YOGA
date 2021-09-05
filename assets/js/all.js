2e930e3c0e0c453fc4b56a6159c64bf51ad01097
sylvia-H <sylvie11gaia@gmail.com>
2021-09-05 20:05:19 +0800

    Update 2021-09-05T12:04:59.434Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,67 @@
 "use strict";
 
-$(function () {
-  console.log('Hello Bootstrap5');
+(function () {
+  'use strict'; // breakpoint where swiper will be destroyed
+  // and switches to a dual-column layout
+
+  var breakpoint = window.matchMedia('(max-width: 992px)'); // keep track of swiper instances to destroy later
+
+  var courseSwiper; //////////////////////////////////////////////////////////////////
+
+  var breakpointChecker = function breakpointChecker() {
+    // if larger viewport and multi-row layout needed
+    if (breakpoint.matches === true) {
+      // clean up old instances and inline styles when available
+      if (courseSwiper !== undefined) courseSwiper.destroy(true, true); // or/and do nothing
+
+      return; // else if a small viewport and single column layout needed
+    } else if (breakpoint.matches === false) {
+      // fire small viewport version of swiper
+      return enableSwiper();
+    }
+  }; //////////////////////////////////////////////////////////////////
+
+
+  var enableSwiper = function enableSwiper() {
+    courseSwiper = new Swiper('.swiper-container', {
+      slidesPerView: 1.5,
+      spaceBetween: 30
+    });
+  }; //////////////////////////////////////////////////////////////////
+  // keep an eye on viewport size changes
+
+
+  breakpoint.addListener(breakpointChecker); // kickstart
+
+  breakpointChecker();
+})();
+/* IIFE end */
+
+
+var customerSwiper = new Swiper(".customerSwiper", {
+  slidesPerView: 1,
+  slidesPerColumn: 3,
+  spaceBetween: 8,
+  slidesPerColumnFill: 'row',
+  navigation: {
+    nextEl: ".swiper-next",
+    prevEl: ".swiper-prev"
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      slidesPerColumn: 2,
+      spaceBetween: 30
+    },
+    996: {
+      slidesPerView: 3,
+      slidesPerColumn: 2,
+      spaceBetween: 30
+    }
+  }
+});
+AOS.init({
+  offset: 100,
+  duration: 800
 });
 //# sourceMappingURL=all.js.map
