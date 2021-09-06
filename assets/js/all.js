5c5a7a10898a298e3de0bd0dbb9ffd0e64f7d4dc
sylvia-H <sylvie11gaia@gmail.com>
2021-09-06 23:19:22 +0800

    Update 2021-09-06T15:19:05.573Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -5,6 +5,10 @@ var courseSwiper = new Swiper('.courseSwiper', {
   spaceBetween: 30
 });
 var customerSwiper = new Swiper(".customerSwiper", {
+  // 添加 observe 參數進行自動初始化
+  observer: true,
+  observeParents: true,
+  loop: true,
   slidesPerView: 3,
   slidesPerColumn: 2,
   spaceBetween: 30,
@@ -28,6 +32,31 @@ var customerSwiper = new Swiper(".customerSwiper", {
       slidesPerView: 1,
       slidesPerColumn: 3,
       spaceBetween: 8
+    }
+  }
+});
+var teacherSwiper = new Swiper(".teacherSwiper", {
+  // 添加 observe 參數進行自動初始化
+  observer: true,
+  observeParents: true,
+  loop: true,
+  initialSlide: 0,
+  slidesPerView: 3.5,
+  spaceBetween: 30,
+  slidesPerColumnFill: 'row',
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false
+  },
+  //Swiper4.x 版本是 min-width 概念
+  breakpoints: {
+    996: {
+      slidesPerView: 2.1,
+      spaceBetween: 30
+    },
+    768: {
+      slidesPerView: 1,
+      spaceBetween: 24
     }
   }
 });
