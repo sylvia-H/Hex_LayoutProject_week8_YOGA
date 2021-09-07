905ab5431833198571f68ace23b5548c5764c1e0
sylvia-H <sylvie11gaia@gmail.com>
2021-09-07 23:13:33 +0800

    瑜珈空間頁

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -3,15 +3,19 @@ const courseSwiper = new Swiper ('.courseSwiper', {
   spaceBetween: 30,
 });
 
+
 const customerSwiper = new Swiper(".customerSwiper",{
   // 添加 observe 參數進行自動初始化
   observer:true,
   observeParents:true,
   loop: true,
-  slidesPerView: 3,
-  slidesPerColumn: 2,
-  spaceBetween: 30,
-  slidesPerColumnFill: 'row',
+  initialSlide: 0,
+  slidesPerView: 1,
+  spaceBetween: 8,
+  grid: {
+    rows: 3,
+    fill: 'column',
+  },
   navigation: {
     nextEl: ".swiper-next",
     prevEl: ".swiper-prev",
@@ -23,17 +27,24 @@ const customerSwiper = new Swiper(".customerSwiper",{
   //Swiper4.x 版本是 min-width 概念
   breakpoints:{
     996: {
-      slidesPerView: 2,
-      slidesPerColumn: 2,
+      slidesPerView: 3,
       spaceBetween: 30,
+      grid: {
+        rows: 2,
+        fill: 'row',
+      },
     },
     768: {
-      slidesPerView: 1,
-      slidesPerColumn: 3,
-      spaceBetween: 8,
+      slidesPerView: 2,
+      spaceBetween: 30,
+      grid: {
+        rows: 2,
+        fill: 'row',
+      },
     }
   }
-})
+});
+
 
 const teacherSwiper = new Swiper(".teacherSwiper",{
   // 添加 observe 參數進行自動初始化
@@ -41,9 +52,12 @@ const teacherSwiper = new Swiper(".teacherSwiper",{
   observeParents:true,
   loop: true,
   initialSlide: 0,
-  slidesPerView: 3.5,
-  spaceBetween: 30,
-  slidesPerColumnFill: 'row',
+  slidesPerView: 1,
+  spaceBetween: 24,
+  grid: {
+    rows: 1,
+    fill: 'row',
+  },
   autoplay: {
     delay: 2000,
     disableOnInteraction: false,
@@ -51,15 +65,60 @@ const teacherSwiper = new Swiper(".teacherSwiper",{
   //Swiper4.x 版本是 min-width 概念
   breakpoints:{
     996: {
-      slidesPerView: 2.1,
+      slidesPerView: 3.5,
       spaceBetween: 30,
+      grid: {
+        rows: 1,
+        fill: 'row',
+      },
     },
     768: {
-      slidesPerView: 1,
-      spaceBetween: 24,
+      slidesPerView: 2.1,
+      spaceBetween: 30,
+      grid: {
+        rows: 1,
+        fill: 'row',
+      },
     }
   }
-})
+});
+
+const recommendSwiper = new Swiper(".recommendSwiper",{
+  // 添加 observe 參數進行自動初始化
+  observer:true,
+  observeParents:true,
+  loop: true,
+  initialSlide: 0,
+  slidesPerView: 1.2,
+  spaceBetween: 24,
+  grid: {
+    rows: 1,
+    fill: 'row',
+  },
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
+  //Swiper4.x 版本是 min-width 概念
+  breakpoints:{
+    996: {
+      slidesPerView: 4.2,
+      spaceBetween: 30,
+      grid: {
+        rows: 1,
+        fill: 'row',
+      },
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      grid: {
+        rows: 1,
+        fill: 'row',
+      },
+    }
+  }
+});
 
 AOS.init({
   offset: 100,
