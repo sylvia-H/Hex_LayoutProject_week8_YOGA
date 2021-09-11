9d6dbf7d98c9d167a097d6b1884e18b1a1c841c4
sylvia-H <sylvie11gaia@gmail.com>
2021-09-11 23:34:34 +0800

    swiper 微調

--- app/assets/js/swiper.js
+++ app/assets/js/swiper.js
@@ -134,7 +134,7 @@ const gradeSwiper = new Swiper ('.gradeSwiper', {
         fill: 'row',
     },
     breakpoints:{
-        996: {
+        1200: {
         slidesPerView: 3,
         spaceBetween: 20,
         grid: {
@@ -142,8 +142,16 @@ const gradeSwiper = new Swiper ('.gradeSwiper', {
             fill: 'row',
             },
         },
-        768: {
+        992: {
         slidesPerView: 2.1,
+        spaceBetween: 20,
+        grid: {
+            rows: 1,
+            fill: 'row',
+            },
+        },
+        768: {
+        slidesPerView: 2,
         spaceBetween: 30,
         grid: {
             rows: 1,
