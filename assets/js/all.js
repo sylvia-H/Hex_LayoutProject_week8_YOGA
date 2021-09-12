f8f052b16447fa2a16696b8aef16752e28ba9374
sylvia-H <sylvie11gaia@gmail.com>
2021-09-13 00:10:14 +0800

    Update 2021-09-12T16:09:54.846Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -8,23 +8,86 @@ AOS.init({
 
 $(function () {
   // 定義變數
-  var assignmentListChild = document.getElementById('assignmentList').getElementsByTagName('a');
-  var pre = document.querySelectorAll('.page-item')[0]; // addEventListener
+  var plans = document.querySelectorAll('.select-plan');
+  var arrows = document.querySelectorAll('.plan__arrow');
+  var course__plan = document.querySelectorAll('.course__plan');
+  var course__planCard = document.querySelectorAll('.course__plan>.card');
+  var course__grades = document.querySelector('.course__grades');
+  var grades = document.querySelectorAll('.select-grade');
+  var course_grade = document.getElementById('course__grade');
+  var course_plan = document.getElementById('course__plan');
+  var course__solution = document.querySelector('.course__solution'); // setTimeout function
 
-  pre.addEventListener('click', function () {
-    if (assignNow <= 0) {
-      assignNow = 0;
-      assignmentListChild[assignNow].classList.add("active");
-      pre.classList.add("disabled");
-      console.log(assignNow);
-    } else {
-      assignNow -= 1;
-      assignmentListChild[assignNow + 1].classList.remove("active");
-      assignmentListChild[assignNow].classList.add("active");
-      pre.classList.remove("disabled");
-      next.classList.remove("disabled");
-      console.log(assignNow);
-    }
+  function reAdd_gradeShow() {
+    course__grades.classList.add('course__grades__show');
+  } // 三個方案加上 addEventListener
+
+
+  plans.forEach(function (el) {
+    el.addEventListener('click', function (e) {
+      // 移除所有 planCard 的 border-4 屬性
+      course__plan.forEach(function (plan) {
+        if (!plan.classList.contains('d-lg-block')) {
+          plan.classList.add('d-none');
+          plan.classList.add('d-lg-block');
+        }
+      }); // 被點擊到的 planCard 加上 border-4 屬性
+
+      e.target.closest('.course__plan').classList.remove('d-none');
+      e.target.closest('.course__plan').classList.remove('d-lg-block'); // 移除所有 planCard 的 border-4 屬性
+
+      course__planCard.forEach(function (planCard) {
+        if (planCard.classList.contains('border-4')) {
+          planCard.classList.remove('border-4');
+        }
+      }); // 被點擊到的 planCard 加上 border-4 屬性
+
+      e.target.closest('.card').classList.toggle('border-4'); // 沒被隱藏的小箭頭加上 d-none 屬性
+
+      arrows.forEach(function (arrow) {
+        if (!arrow.classList.contains('d-none')) {
+          arrow.classList.add('d-none');
+        }
+      }); // 被點擊到的小箭頭移除 d-none 屬性
+
+      e.target.closest('.course__plan').childNodes[3].classList.toggle('d-none'); // 選擇課程階級的 grades 區塊 slideup & slidedown
+
+      if (course__grades.classList.contains('course__grades__show')) {
+        course__grades.classList.remove('course__grades__show');
+        setTimeout(reAdd_gradeShow, 800);
+      } else {
+        course__grades.classList.add('course__grades__show');
+      }
+
+      course_plan.innerText = e.target.closest('.card-body').childNodes[1].innerText;
+    });
+  }); // 三個課程階級加上 addEventListener
+
+  grades.forEach(function (el) {
+    el.addEventListener('click', function (e) {
+      // 移除所有 select-grade 的 border-4 屬性
+      grades.forEach(function (el) {
+        if (el.classList.contains('border-4')) {
+          el.classList.remove('border');
+          el.classList.remove('border-white');
+          el.classList.remove('border-4');
+        } // 所有階級 check_circle 加上 opacity-50 屬性
+
+
+        if (!el.childNodes[1].childNodes[3].classList.contains('opacity-50')) {
+          el.childNodes[1].childNodes[3].classList.add('opacity-50');
+        }
+      }); // 被點擊到的 select-grade 加上 border-4 屬性
+
+      e.target.closest('.select-grade').classList.add('border');
+      e.target.closest('.select-grade').classList.add('border-white');
+      e.target.closest('.select-grade').classList.add('border-4'); // 被點擊到的階級 check_circle 移除 opacity-50 屬性
+
+      e.target.closest('.select-grade').childNodes[1].childNodes[3].classList.remove('opacity-50');
+      course_grade.innerText = e.target.closest('.select-grade').childNodes[1].childNodes[1].innerText.split(" ")[0]; // 顯示方案結果
+
+      course__solution.classList.remove('d-none');
+    });
   });
 });
 "use strict";
@@ -36,11 +99,6 @@ var courseSwiper = new Swiper('.courseSwiper', {
 }); // 首頁 - 聽聽他們怎麼說 - 顧客意見
 
 var customerSwiper = new Swiper(".customerSwiper", {
-  // 添加 observe 參數進行自動初始化
-  observer: true,
-  observeParents: true,
-  loop: true,
-  initialSlide: 0,
   slidesPerView: 1,
   spaceBetween: 8,
   grid: {
@@ -55,7 +113,6 @@ var customerSwiper = new Swiper(".customerSwiper", {
     delay: 3000,
     disableOnInteraction: false
   },
-  //Swiper4.x 版本是 min-width 概念
   breakpoints: {
     996: {
       slidesPerView: 3,
@@ -80,8 +137,8 @@ var teacherSwiper = new Swiper(".teacherSwiper", {
   // 添加 observe 參數進行自動初始化
   observer: true,
   observeParents: true,
-  loop: true,
   initialSlide: 0,
+  loop: true,
   slidesPerView: 1,
   spaceBetween: 24,
   grid: {
