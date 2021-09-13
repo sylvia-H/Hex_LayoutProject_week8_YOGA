e242a7e37f8aefa9baaf2116f86d02f9bb8e9823
sylvia-H <sylvie11gaia@gmail.com>
2021-09-13 18:10:21 +0800

    預約頁面完成 - 待修

--- app/assets/js/reserve.js
+++ app/assets/js/reserve.js
@@ -9,7 +9,18 @@ $(function() {
   let course_grade = document.getElementById('course__grade');
   let course_plan = document.getElementById('course__plan');
   const course__solution = document.querySelector('.course__solution');
-  
+  const btn_reserve_continue = document.getElementById('btn-reserve-continue');
+  let course__solution_final
+  const reserveInfo__solution_final = document.getElementById('reserveInfo__solution_final');
+  const btn_reserveform = document.getElementById('btn-reserveform');
+  const checked_course = document.getElementById('checked_course');
+  const checked_date = document.getElementById('checked_date');
+  const checked_name = document.getElementById('checked_name');
+  const checked_age = document.getElementById('checked_age');
+  const checked_gender = document.getElementById('checked_gender');
+  const checked_email = document.getElementById('checked_email');
+  const checked_phone = document.getElementById('checked_phone');
+
   // setTimeout function
   function reAdd_gradeShow(){
     course__grades.classList.add('course__grades__show');
@@ -87,8 +98,40 @@ $(function() {
 
       // 顯示方案結果
       course__solution.classList.remove('d-none');
+
+      // 顯示'繼續預約'按鈕
+      btn_reserve_continue.classList.remove('d-none');
+
+      // 把最終方案結果存入 sessionStorage
+      course__solution_final = document.getElementById('course__solution-final').innerText;
+      window.sessionStorage.setItem('solution_final',course__solution_final);
     });
   });
 
+  // 從 sessionStorage 取值代入 第二預約頁-填寫資料
+  reserveInfo__solution_final.innerText = window.sessionStorage.getItem('solution_final');
+
+  // 完成填寫預約表單，按鈕加上 addEventListener，並把資料存入 sessionStorage
+    let form_date = $('#form_date').val();
+    let form_name = $('#form_name').val();
+    let form_age = $('#form_age').val();
+    let form_gender = $('#form_gender').val();
+    let form_email = $('#form_email').val();
+    let form_phone = $('#form_phone').val();
+    window.sessionStorage.setItem('form_date',form_date);
+    window.sessionStorage.setItem('form_name',form_name);
+    window.sessionStorage.setItem('form_age',form_age);
+    window.sessionStorage.setItem('form_gender',form_gender);
+    window.sessionStorage.setItem('form_email',form_email);
+    window.sessionStorage.setItem('form_phone',form_phone);
+  
+  // 從 sessionStorage 取值代入 第三預約頁-完成預約
+  checked_course.innerText = window.sessionStorage.getItem('solution_final');
+  checked_date.innerText = window.sessionStorage.getItem('form_date');
+  checked_name.innerText = window.sessionStorage.getItem('form_name');
+  checked_age.innerText = window.sessionStorage.getItem('form_age');
+  checked_gender.innerText = window.sessionStorage.getItem('form_gender');
+  checked_email.innerText = window.sessionStorage.getItem('form_email');
+  checked_phone.innerText = window.sessionStorage.getItem('form_phone');
 
 });
